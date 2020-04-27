69dea516681c001378cb5ce03560ff83038642f2
Alexandre Tagliari <56889964+AlexandreTcol@users.noreply.github.com>
2020-04-27 16:39:13 -0300

    BackEnd

---
+++ backEnd/src/controllers/OngController.js
@@ -0,0 +1,33 @@
+const genereteUniqueId = require('../utils/generateUniquieid')
+const connection = require('../database/connection');
+
+
+
+module.exports={
+
+    async index(request,response){
+        const ongs = await connection('ongs').select('*');
+        return response.json(ongs);
+    },
+    
+
+    async create (request, response){
+        const {name, email , whatsapp, city, uf} = request.body;
+
+     const id = genereteUniqueId()
+
+     await  connection('ongs').insert({
+         id,
+         name,
+         email,
+         whatsapp,
+         city,
+         uf
+     })
+
+
+     
+
+    return response.json({id});
+    }
+};
